6209ffb678e063155314bb3aae45c9b84ba49762
khadijaholowu <khadijaholowu@wirelessprv-10-192-87-193.near.illinois.edu>
2017-09-26 14:33:06 -0500

    Getting started with Lab 4

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -12,15 +12,15 @@ public class ReclamationProject
 {
     static String doit(String a,String b){
         if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
+            String c = a;
             a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
+        String r = (a.equals(b)) ? "" : "";
+
+
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
                 for (int k = 0; k < b.length()- j; k++) {
                     r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
+                        }}
         } return r; }
 }
